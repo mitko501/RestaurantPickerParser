ca23437224fa4a17baffd9adf32ff1cdcbfed623
mitko501 <422190@mail.muni.cz>
2018-03-22 19:13:51 +0100

    Remove hardcoded days in DayNameParser
    Still needs some work to do in this parser e.g.
    - Move day names to property file
    - Maybe use another parser to parse days for example in case there is price in different element we need to use them in one FoodEntity not two different

--- src/main/java/pv239/fi/muni/cz/dailymenupicker/parser/day/DayNameDayParser.kt
+++ src/main/java/pv239/fi/muni/cz/dailymenupicker/parser/day/DayNameDayParser.kt
@@ -1,23 +1,44 @@
 package pv239.fi.muni.cz.dailymenupicker.parser.day
 
 import org.jsoup.nodes.Element
-import org.jsoup.nodes.TextNode
-import pv239.fi.muni.cz.dailymenupicker.parser.Parser
 import pv239.fi.muni.cz.dailymenupicker.parser.TodayMenuParser
 import pv239.fi.muni.cz.dailymenupicker.parser.config.Day
 import pv239.fi.muni.cz.dailymenupicker.parser.entity.FoodEntity
 import pv239.fi.muni.cz.dailymenupicker.parser.entity.TodayMenuEntity
+import java.util.Calendar
+
+
 
 /**
  * Created by mhajas on 3/17/18.
  */
 class DayNameDayParser(val config: Day) : TodayMenuParser<Element>() {
+
+    private val days: Array<String> = arrayOf("PONDĚLÍ", "ÚTERÝ", "STŘEDA", "ČTVRTEK", "PÁTEK") // TODO: Move to property file, possible future localization
+
     override fun parse(t: Element): TodayMenuEntity {
         var menu = t.textNodes().map { textNode -> textNode.text() }
-        menu = menu.dropWhile{ s -> !s.startsWith("ÚTERÝ")}.takeWhile { s -> !s.startsWith("STŘEDA") }
+        menu = menu.dropWhile{ s -> !containsTodayDayName(s)}.takeWhile { s -> !containsTomorrow(s) }
 
         return TodayMenuEntity(
                 menu.first(),
                 menu.drop(1).map { s -> FoodEntity(s) })
     }
+
+
+    private fun containsTodayDayName(dayName: String): Boolean{
+        val calendar = Calendar.getInstance()
+        val todayNumber = calendar.get(Calendar.DAY_OF_WEEK)
+
+        return dayName.contains(days[todayNumber - 2], true) // -1 indexing from 0 and -1 for starting with sunday in Calendar
+    }
+
+    private fun containsTomorrow(dayName: String): Boolean{
+        val calendar = Calendar.getInstance()
+        val tomorrowNumber = calendar.get(Calendar.DAY_OF_WEEK) + 1
+
+        return dayName.contains(days[tomorrowNumber - 2], true) // -1 indexing from 0 and -1 for starting with sunday in Calendar
+    }
+
+
 }
